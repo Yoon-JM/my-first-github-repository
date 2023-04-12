2f0794b293426bbeb681cb64d594dc20b8b2c3b5
Yoon-JM <wlals3591@gmail.com>
2023-04-12 15:19:00 +0900

    2 commit

--- src/main/java/com/example/test/github/TEST/test.java
+++ src/main/java/com/example/test/github/TEST/test.java
@@ -5,5 +5,7 @@ public class test {
         System.out.println("모든것이여 망해라");
         System.out.println("모든것이여 망해라1");
         System.out.println("모든것이여 망해라2");
+        System.out.println("개같이 하기싫어");
+        System.out.println("다 포기하고 싶어");
     }
 }
